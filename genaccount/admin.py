e52fc862fe2344b73b70c9089959a70588c896d8
Chris Lombaard <lombaardcjll@gmail.com>
2015-10-21 23:56:39 +0200

    Updated mistakes in model.py. Started testing Admin page

--- genaccount/admin.py
+++ genaccount/admin.py
@@ -3,7 +3,32 @@ from django.contrib import admin
 from .models import Account, BasicJournal, JournalEntry
 
 # Register your models here.
+class JournalEntryInline(admin.TabularInline):
+    model=JournalEntry
+    
+    #determine default number of choices to add automatically everytime admin form is opened to add/edit entry
+    extra=0
+
+class BasicJournalAdmin(admin.ModelAdmin):
+    #define inline
+    inlines=[JournalEntryInline]
+
+    list_display = ('comment',)
+    
+    
+    #Which field can be search on
+    search_fields=['comment']
+    
+    #Own research. How to make fields editable in change list view
+    list_editable =['comment']
+    
+    #Pagination. Items list per page
+    list_per_page = 5
+    
+    #Limit the maximum amount of items to display. Protect the user against themselves
+    list_max_show_all = 10
+    
 
 admin.site.register(Account)
-admin.site.register(BasicJournal)
-admin.site.register(JournalEntry)
+#admin.site.register(BasicJournal)
+admin.site.register(BasicJournal, BasicJournalAdmin)
--- genaccount/models.py
+++ genaccount/models.py
@@ -40,7 +40,26 @@ class Account(models.Model):
     
     
     def __str__(self):
-        return self.name + ' (' + str(self.id) + ')'
+        ASSET = 1
+        LIABILITY = 2
+        EQUITY = 3
+        INCOME = 4
+        COST_OF_SALES = 5
+        EXPENSE = 6
+        OTHER_INCOME = 7
+        OTHER_EXPENSE = 8
+            
+        TYPE_CHOICES = (
+            (ASSET, 'Asset'),
+            (LIABILITY, 'Liability'),
+            (EQUITY, 'Equity'),
+            (INCOME, 'Income'),
+            (COST_OF_SALES, 'Cost of Sales'),
+            (EXPENSE, 'Expense'),
+            (OTHER_INCOME, 'Other Income'),
+            (OTHER_EXPENSE, 'Other Expense')
+        )
+        return '[' + str(self.type) + '](' + str(self.id) + ') ' + self.name + ' ' + TYPE_CHOICES[self.type][1]
 
 
 class BasicJournal(models.Model):
@@ -73,4 +92,4 @@ class JournalEntry(models.Model):
     
     
     def __str__(self):
-        return self + ' (' + str(self.id) + ')'
+        return self.descr + ' (' + str(self.id) + ')'
